c3a4a1de1536b34ae07f7e41b885615976ed67c3
Yuri Jean Fabris <yuri@blocknomad.xyz>
2022-12-21 09:32:17 -0300

    chore: add NEXT_PUBLIC_FFDST_CONTRACT_ADDRESS env var

--- app.config.js
+++ app.config.js
@@ -88,7 +88,7 @@ module.exports = {
   /// Fractal Flows
   fractalflowsBaseToken: {
     5: {
-      address: '0x819b194B69bC7a56c0571C2C5520c594eFab2793',
+      address: process.env.NEXT_PUBLIC_FFDST_CONTRACT_ADDRESS,
       symbol: 'FFDST',
       name: 'Fractal Flows Decentralized Science Token',
       decimals: 18
